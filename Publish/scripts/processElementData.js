96fab225a84fb80b3329c20d5c78be435bc05e4d
rutefernandes <rutefdn@hotmail.com>
2019-02-24 02:31:21 -0300

    iniciando desenvolvimento do ciclo de vida do projeto

---
+++ Publish/scripts/processElementData.js
@@ -0,0 +1,52 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_6EZlATfVEemwn90hXFEE7g,_ZzTp8TfaEemwn90hXFEE7gActivityDetail"]="./../../xp/deliveryprocesses/resources/Implementação_37FA12FD_7f4f99bf_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_6EZlATfVEemwn90hXFEE7g,_XT2k8TfaEemwn90hXFEE7gActivityDetail"]="./../../xp/deliveryprocesses/resources/Planejamento_8467CB36_cbbd51f8_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_6EZlATfVEemwn90hXFEE7g,_Yo66cDfaEemwn90hXFEE7gActivityDetail"]="./../../xp/deliveryprocesses/resources/Elaboração_6333A21C_aa8928de_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_6EZlATfVEemwn90hXFEE7gActivity"]="./../../xp/deliveryprocesses/resources/Ciclo de vida_F555E26E_f555e26e_Activity.jpeg"
+contentPage.processPage.imageFiles["_6EZlATfVEemwn90hXFEE7g,_a2gzYTfaEemwn90hXFEE7gActivity"]="./../../xp/deliveryprocesses/resources/Testes_EB1601A4_326b8866_Activity.jpeg"
+contentPage.processPage.elementUrls["_Qw4CUTfpEemUQtxHzDLDiA"] = ["Construção XP","xp/deliveryprocesses/construcao_xp_523C83F2.html"];
+contentPage.processPage.elementUrls["_XT2k8TfaEemwn90hXFEE7g"] = ["Planejamento","xp/deliveryprocesses/Planejamento_8467CB36.html"];
+contentPage.processPage.elementUrls["_a2gzYTfaEemwn90hXFEE7g"] = ["Testes","xp/deliveryprocesses/Testes_EB1601A4.html"];
+contentPage.processPage.elementUrls["_lAA8gDfmEemUQtxHzDLDiA"] = ["Cliente XP","xp/deliveryprocesses/cliente_xp_810AE064.html"];
+contentPage.processPage.elementUrls["_lABjkTfmEemUQtxHzDLDiA"] = ["Plano de Iteração XP","xp/deliveryprocesses/plano_de_iteracao_xp_B0D93701.html"];
+contentPage.processPage.elementUrls["_jIxcIDfoEemUQtxHzDLDiA"] = ["Implantar o Ambiente de Testes","xp/deliveryprocesses/implantar_ambiente_de_testes_D2C8E5E2.html"];
+contentPage.processPage.elementUrls["_hJEt4DfrEemUQtxHzDLDiA"] = ["Validação de sistema corporativo","xp/deliveryprocesses/Validação de sistema corporativo_4303E141.html"];
+contentPage.processPage.elementUrls["_lABjkDfmEemUQtxHzDLDiA"] = ["Plano de Liberação XP","xp/deliveryprocesses/plano_de_liberacao_xp_A63B56F1.html"];
+contentPage.processPage.elementUrls["_fmdu0DfvEemUQtxHzDLDiA"] = ["Programador XP","xp/deliveryprocesses/programador_xp_1E4B7470.html"];
+contentPage.processPage.elementUrls["_eoZTIDfmEemUQtxHzDLDiA"] = ["Revisar requisitos e casos de uso","xp/deliveryprocesses/Revisar requisitos e casos de uso_2B3703FA.html"];
+contentPage.processPage.elementUrls["_Yo66cDfaEemwn90hXFEE7g"] = ["Elaboração","xp/deliveryprocesses/Elaboração_6333A21C.html"];
+contentPage.processPage.elementUrls["_ZzTp8TfaEemwn90hXFEE7g"] = ["Implementação","xp/deliveryprocesses/Implementação_37FA12FD.html"];
+contentPage.processPage.elementUrls["_hDi1ADfoEemUQtxHzDLDiA"] = ["Atualizar plano de implementação","xp/deliveryprocesses/atualizar_plano_de_implementao_A46EECE4.html"];
+contentPage.processPage.elementUrls["_jluZsDfvEemUQtxHzDLDiA"] = ["Realizar testes de integração das novas funcionalidades","xp/deliveryprocesses/realizar_testes_de_integrao_das_novas_funcionalidades_4EBE847C.html"];
+contentPage.processPage.elementUrls["_kR6gYDfmEemUQtxHzDLDiA"] = ["Adaptar e Melhorar o Processo","xp/deliveryprocesses/adaptar_e_melhorar_o_processo_DF06D1FE.html"];
+contentPage.processPage.elementUrls["_d_pukDfaEemwn90hXFEE7g"] = ["Teste de aceitação ","xp/deliveryprocesses/Teste de aceitação_9D8AFFDE.html"];
+contentPage.processPage.elementUrls["_knJacDfvEemUQtxHzDLDiA"] = ["Realizar testes de aceitação das novas funcionalidades ","xp/deliveryprocesses/realizar_testes_de_aceitao_das_novas_funcionalidades__5CD1B27F.html"];
+contentPage.processPage.elementUrls["_WsJ1UDfmEemUQtxHzDLDiA"] = ["Treinador XP","xp/deliveryprocesses/treinador_xp_A0B87B6D.html"];
+contentPage.processPage.elementUrls["_Qw4CUDfpEemUQtxHzDLDiA"] = ["Código de Produção","xp/deliveryprocesses/codigo_de_producao_xp_479EA3E2.html"];
+contentPage.processPage.elementUrls["_XgYAMDfpEemUQtxHzDLDiA"] = ["Acompanhar o Progresso da Iteração","xp/deliveryprocesses/acompanhar_progresso_da_iteracao_E6378D6C.html"];
+contentPage.processPage.elementUrls["_d1W9kDfvEemUQtxHzDLDiA"] = ["Correção de erros ","xp/deliveryprocesses/Correção de erros_73DEC60C.html"];
+contentPage.processPage.elementUrls["_Qw3bQTfpEemUQtxHzDLDiA"] = ["Programador XP","xp/deliveryprocesses/programador_xp_3FF4FB76.html"];
+contentPage.processPage.elementUrls["_agOj4DfmEemUQtxHzDLDiA"] = ["Melhorar as Habilidades da Equipe","xp/deliveryprocesses/melhorar_habilidades_da_equipe_4511116C.html"];
+contentPage.processPage.elementUrls["_fmdHwDfvEemUQtxHzDLDiA"] = ["Realizar testes de unidade no sistema","xp/deliveryprocesses/Realizar testes de unidade no sistema_7BCDFC1C.html"];
+contentPage.processPage.elementUrls["_eoZTITfmEemUQtxHzDLDiA"] = ["Programador XP","xp/deliveryprocesses/programador_xp_35D4E40A.html"];
+contentPage.processPage.elementUrls["_k__uYDfmEemUQtxHzDLDiA"] = ["Definir Iteração","xp/deliveryprocesses/definir_plano_de_iteracao_DB07F750.html"];
+contentPage.processPage.elementUrls["_fBOTwDfvEemUQtxHzDLDiA"] = ["Implementar requisitos planejados","xp/deliveryprocesses/implementar_requisitos_planejados_1D2BEB2.html"];
+contentPage.processPage.elementUrls["_Qw3bQDfpEemUQtxHzDLDiA"] = ["Realizar testes de unidade no sistema","xp/deliveryprocesses/Realizar testes de unidade no sistema_35571B66.html"];
+contentPage.processPage.elementUrls["_fme88TfvEemUQtxHzDLDiA"] = ["Construção XP","xp/deliveryprocesses/construcao_xp_3380F0DA.html"];
+contentPage.processPage.elementUrls["_fme88DfvEemUQtxHzDLDiA"] = ["Código de Produção","xp/deliveryprocesses/codigo_de_producao_xp_28E310CA.html"];
+contentPage.processPage.elementUrls["_XgYnQDfpEemUQtxHzDLDiA"] = ["Rastreador XP","xp/deliveryprocesses/rastreador_xp_1F7244D5.html"];
+contentPage.processPage.elementUrls["_WsJOQDfmEemUQtxHzDLDiA"] = ["Explicar o Processo","xp/deliveryprocesses/explicar_processo_A868678F.html"];
+contentPage.processPage.elementUrls["_6EZlATfVEemwn90hXFEE7g"] = ["Ciclo de vida","xp/deliveryprocesses/Ciclo de vida_F555E26E.html"];
+contentPage.processPage.elementUrls["_jIyDMDfoEemUQtxHzDLDiA"] = ["Testador XP","xp/deliveryprocesses/testador_xp_E5106E5E.html"];
